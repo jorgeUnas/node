10361b19225bbf25f366877246f31e52a35150ce
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-04-30 16:06:37 +0000

    use the console methods in an Array

--- basics/app.js
+++ basics/app.js
@@ -6,3 +6,12 @@ let noun3 = 'man';
 
 
 console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
+
+
+const petsArray = ['dog', 'cat', 'bird', 'monkey'];
+
+// Add console methods below!
+
+console.log(petsArray);
+console.table(petsArray);
+console.assert(petsArray.length > 5);
