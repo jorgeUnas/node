fa10145443dfd4b0f0a67e6ab512f74a1a9f91db
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-04-30 15:53:47 +0000

    run my first programm with node

---
+++ basics/app.js
@@ -0,0 +1,8 @@
+let noun1 = 'cat';
+let adjective = 'stupid';
+let noun2 = 'dog';
+let verb = 'run';
+let noun3 = 'man';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
