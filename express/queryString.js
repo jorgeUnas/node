fc78d7bd073a36e4d9f05ced8177cacab1934b23
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-05-21 10:50:08 +0000

    How to set a query string

---
+++ express/queryString.js
@@ -0,0 +1,7 @@
+const monsters = { '1': { name: 'cerberus', age: '4'  } };
+// PUT /monsters/1?name=chimera&age=1
+app.put('/monsters/:id', (req, res, next) => {
+  const monsterUpdates = req.query;
+  monsters[req.params.id] = monsterUpdates;
+  res.send(monsters[req.params.id]);
+});
