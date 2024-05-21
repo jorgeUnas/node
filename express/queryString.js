482d27aaccf95dc6b0c47792292d901fdcc541db
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-05-21 13:03:48 +0000

    proof your knowledge on PUT requests

--- express/queryString.js
+++ express/queryString.js
@@ -1,3 +1,4 @@
+/*
 const monsters = { '1': { name: 'cerberus', age: '4'  } };
 // PUT /monsters/1?name=chimera&age=1
 app.put('/monsters/:id', (req, res, next) => {
@@ -5,3 +6,42 @@ app.put('/monsters/:id', (req, res, next) => {
   monsters[req.params.id] = monsterUpdates;
   res.send(monsters[req.params.id]);
 });
+*/
+
+// App.js
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, getIndexById, updateElement,
+        seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
