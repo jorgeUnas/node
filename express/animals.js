93d9fbbba99d90f02633ccce51bc945104419189
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-05-22 12:56:43 +0000

    DELETE request for animals array with anamals/:id route

--- express/animals.js
+++ express/animals.js
@@ -89,6 +89,16 @@ app.delete('/expressions/:id', (req, res, next) => {
   }
 });
 
+app.delete('/animals/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, animals);
+  if (expressionIndex !== -1) {
+    animals.splice(expressionIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`); 
 });
