8ef3f132a65aec514cd6f1ec9bc98e9241054226
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-05-29 10:32:18 +0000

    Fix the GET request to get the metal materials

--- express/practice.js
+++ express/practice.js
@@ -4,13 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 const sausageTypes = ['bratwurst', 'andouille', 'chorizo', 'boudin', 'kielbasa'];
-
+const buildingMaterials = {
+  wood: ['plywood', '2x4s', 'cedar shingles'],
+  metal: ['steel girders', 'wall studs', 'rebar'],
+};
 
 // GET request
 
 app.get('/sausages', (req, res, next) => {
   res.send(sausageTypes)
 })
+
+app.get('/metals', (req, res, next) => {
+  const arrayToSend = buildingMaterials.metal;
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`)
