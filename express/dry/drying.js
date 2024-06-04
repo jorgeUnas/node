c695ff9e9696af6a3593c744a8e54e5aca6a2ea5
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-04 09:20:47 +0000

    Eliminate the redundant code in drying.js

--- express/dry/drying.js
+++ express/dry/drying.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 4001;
-
-const express = require('express');
-const app = express();
-
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4001;
@@ -34,7 +29,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Add your code below:
 app.use('/beans/:beanName', (req, res, next) => {
   const beanName = req.params.beanName;
   if (!jellybeanBag[beanName]) {
@@ -46,19 +40,19 @@ app.use('/beans/:beanName', (req, res, next) => {
   next();
 });
 
-
+// Add your code below:
 app.use(['/beans/', '/beans/:beanName'], (req, res, next) => {
   let bodyData = '';
   req.on('data', (data) => {
-  bodyData += data;
+    bodyData += data;
   });
   req.on('end', () => {
     if (bodyData) {
-    req.body = JSON.parse(bodyData);
+      req.body = JSON.parse(bodyData);
     }
-  next();
-  })
-})
+    next();
+  });
+});
 
 app.get('/beans/', (req, res, next) => {
   res.send(jellybeanBag);
@@ -66,12 +60,7 @@ app.get('/beans/', (req, res, next) => {
 });
 
 app.post('/beans/', (req, res, next) => {
-  let bodyData = '';
-  req.on('data', (data) => {
-    bodyData += data;
-  });
-
-  req.on('end', () => {
+ 
     const body = JSON.parse(bodyData);
     const beanName = body.name;
     if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
@@ -83,7 +72,6 @@ app.post('/beans/', (req, res, next) => {
     };
     res.send(jellybeanBag[beanName]);
     console.log('Response Sent');
-  });
 });
 
 app.get('/beans/:beanName', (req, res, next) => {
@@ -92,27 +80,16 @@ app.get('/beans/:beanName', (req, res, next) => {
 });
 
 app.post('/beans/:beanName/add', (req, res, next) => {
-  let bodyData = '';
-  req.on('data', (data) => {
-    bodyData += data;
-  });
 
-  req.on('end', () => {
     const numberOfBeans = Number(JSON.parse(bodyData).number) || 0;
     req.bean.number += numberOfBeans;
     res.send(req.bean);
     console.log('Response Sent');
   });
-});
 
 app.post('/beans/:beanName/remove', (req, res, next) => {
-  let bodyData = '';
-  req.on('data', (data) => {
-    bodyData += data;
-  });
 
-  req.on('end', () => {
-    const numberOfBeans = Number(JSON.parse(bodyData).number) || 0;
+      const numberOfBeans = Number(JSON.parse(bodyData).number) || 0;
     if (req.bean.number < numberOfBeans) {
       return res.status(400).send('Not enough beans in the jar to remove!');
     }
@@ -120,28 +97,30 @@ app.post('/beans/:beanName/remove', (req, res, next) => {
     res.send(req.bean);
     console.log('Response Sent');
   });
-});
 
 app.delete('/beans/:beanName', (req, res, next) => {
+  const beanName = req.params.beanName;
+  if (!req.bean) {
+    return res.status(404).send('Bean with that name does not exist');
+  }
   req.bean = null;
   res.status(204).send();
   console.log('Response Sent');
 });
 
 app.put('/beans/:beanName/name', (req, res, next) => {
-  let bodyData = '';
-  req.on('data', (data) => {
-    bodyData += data;
-  });
+  const beanName = req.params.beanName;
+  if (!req.bean) {
+    return res.status(404).send('Bag with that name does not exist');
+  }
 
-  req.on('end', () => {
+
     const newName = JSON.parse(bodyData).name;
-    jellybeanBag[newName] = jellybeanBag[req.beanName];
-    jellybeanBag[req.beanName] = null;
+    jellybeanBag[newName] = req.bean;
+    req.bean = null;
     res.send(jellybeanBag[newName]);
     console.log('Response Sent');
   });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
