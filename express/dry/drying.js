6c6049b3877f51a58369a47ee97245ed68ad44c1
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-04 08:45:10 +0000

    Fill the  middleware

--- express/dry/drying.js
+++ express/dry/drying.js
@@ -48,7 +48,16 @@ app.use('/beans/:beanName', (req, res, next) => {
 
 
 app.use(['/beans/', '/beans/:beanName'], (req, res, next) => {
-
+  let bodyData = '';
+  req.on('data', (data) => {
+  bodyData += data;
+  });
+  req.on('end', () => {
+    if (bodyData) {
+    req.body = JSON.parse(bodyData);
+    }
+  next();
+  })
 })
 
 app.get('/beans/', (req, res, next) => {
