17b99fa6d330d28838ffed44e042080c331e3605
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-11 12:38:13 +0000

    DRY principle applied to challenge3.js

--- express/dry/challenge3.js
+++ express/dry/challenge3.js
@@ -8,19 +8,23 @@ const database = {
 };
 
 // Add your code here:
-const timeMiddleware = () => {};
+const timeMiddleware = (req, res, next) => {
+  req.date = Date.now();
+  next();
+};
+
+app.use(timeMiddleware);
 
 app.get('/snacks', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Snacks as of ${currentTime}: ${database.snacks}`);
+  res.send(`Snacks as of ${req.date}: ${database.snacks}`);
 });
 
 app.get('/sides', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Sides as of ${currentTime}: ${database.sides}`);
+  res.send(`Sides as of ${req.date}: ${database.sides}`);
 });
 
 app.get('/appetizers', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Appetizers as of ${currentTime}: ${database.appetizers}`);
+  res.send(`Appetizers as of ${req.date}: ${database.appetizers}`);
 });
+
+
