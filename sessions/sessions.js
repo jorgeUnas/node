026a9aaed86bc0fb5ae25b13ff5ea65154158098
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-19 12:29:25 +0000

    express-session imported in a variable session

--- sessions/sessions.js
+++ sessions/sessions.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 // Import express-session below:
+const session = require('express-session');
 
 const PORT = process.env.PORT || 4001;
 
