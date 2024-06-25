d9fad64e6afaba1c7c143ec3d6ec079c8ee73101
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-25 12:24:16 +0000

    Hash returned from the plaintext password and salt using the hash() property

--- bcrypt/bcrypt.js
+++ bcrypt/bcrypt.js
@@ -5,6 +5,8 @@ const bcrypt = require("bcrypt");
 const passwordHash = async (password, saltRounds) => {
   try {
     const salt = await bcrypt.genSalt(saltRounds); 
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
   }
   catch(err){
 
