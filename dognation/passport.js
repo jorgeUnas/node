c895bd39e8eefee740decc4e365f72493d4b68d0
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-06-27 12:58:53 +0000

    passport strategy configured with a LocalStrategy instance

--- dognation/passport.js
+++ dognation/passport.js
@@ -4,6 +4,17 @@ const bcrypt = require("bcrypt");
 const helper = require("../helpers/helper");
 
 // Set up the Passport strategy:
+passport.use(
+new LocalStrategy(function (username, password, done) {
+
+    db.users.findByUsername(username, (err, user) => {
+       if(err) return done(err);
+       if(!user) return done(null, false);
+       if(user.password != password) return done(null, false);
+       return done(null, user);
+    });
+  })
+);
 
 // Serialize a user
 
