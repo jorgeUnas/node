87a6b7bef94585517795c5577085c334bf9f9f0a
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-07-11 13:44:04 +0000

    Setting up the variables

--- testing/quoteKeeper/test.js
+++ testing/quoteKeeper/test.js
@@ -2,6 +2,13 @@ const {assert} = require('chai');
 
 describe('User visits root', () => {
     describe('posting a quote', () => {
-    
+        it('saves quote and metadata submitted by user', () => {
+          const quote = 
+          'Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure.';
+          const attributed =
+          'Marianne Williamson';
+          const source = 
+          'A Return to Love: Reflections on the Principles of A Course in Miracles.'
+        })
   })
 })
