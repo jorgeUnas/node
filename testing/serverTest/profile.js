b60f4ff48a5ca6caa34d2788407e8054e46f3a7a
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-07-18 16:12:44 +0000

    Using the username variable to pass the correct message to alice

--- testing/serverTest/profile.js
+++ testing/serverTest/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/:username', (req, res) => {
-    res.send('<h1 id="welcome-message">Welcome alice!</h1>');
+    res.send(`<h1 id="welcome-message">Welcome ${req.params.username}!</h1>`);
 });
 
 router.get('/bob', (req, res) => {
