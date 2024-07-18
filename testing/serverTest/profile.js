1046c255dfe5c52f57080ec99512c74c2d0a8c05
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-07-18 16:18:13 +0000

    Eliminating the profile route for Bob, the test still pass

--- testing/serverTest/profile.js
+++ testing/serverTest/profile.js
@@ -6,8 +6,5 @@ router.get('/:username', (req, res) => {
     res.send(`<h1 id="welcome-message">Welcome ${req.params.username}!</h1>`);
 });
 
-router.get('/bob', (req, res) => {
-    res.send('<h1 id="welcome-message">Welcome bob!</h1>');
-});
 
 module.exports = router;
