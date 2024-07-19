a65e451c4ea78f00bbd686c9816b14d769b8ebe7
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-07-19 09:39:39 +0000

    Updating the response in the GET request

--- testing/serverTest/profile.js
+++ testing/serverTest/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/:username', (req, res) => {
-    res.send(`<h1 id="welcome-message">Welcome ${req.params.username}!</h1>`);
+    res.render('profile', {username});
 });
 
 
