63b70e26d368a2e87682de312ca053e769c3b739
EC2 Default User <ec2-user@ip-172-31-15-186.eu-north-1.compute.internal>
2024-07-23 12:40:00 +0000

    Setting the qoute keeper files

---
+++ testing/serverTest/qouteKeeper/index.js
@@ -0,0 +1,9 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    res.render('index');
+});
+
+module.exports = router;
